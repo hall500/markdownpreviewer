ec88d3c1b36e0cfb0cbcc26490e4bd03dc604530
hall500 <hallhomoms22@gmail.com>
2020-08-01 12:12:32 +0100

    Working MarkdownPreviewer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
+import placeholder from './Sample';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Editor = (props) => {
+	return (
+	<div className="Editor">
+	<h3 className="Header">Editor</h3>
+	<textarea 
+	id="editor" 
+	value={props.markdown}
+	onChange={props.onChange}
+	type="text"
+	>
+	</textarea>
+	</div>
+	);
+}
+
+const Previewer = (props) => {
+	return (
+	<div id="preview" className="Previewer">
+	<h3 className="Header">Preview</h3>
+	<ReactMarkdown className="Markdown"
+	  source={props.markdown}
+	  escapeHtml={false}
+	/>
+	</div>
+	);
+}
+
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			markdown: placeholder,
+			loaded: false
+		}
+		this.handleChange = this.handleChange.bind(this);
+	}
+	
+	handleChange(e){
+		this.setState({
+			markdown: e.target.value
+		});
+	}
+	
+
+	
+	render(){
+		  return (
+			<div className="App">
+			  <Editor markdown={ this.state.markdown } onChange={this.handleChange} />
+			  <Previewer markdown={ this.state.markdown } />
+			</div>
+		  );
+	  }
 }
 
 export default App;
